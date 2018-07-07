62784e06daf869d345c073e1886d5c3d5c452004
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 16:42:56 +0200

    Implement titlecasing algorytm

---
+++ src/main/java/algo/TitleCase.java
@@ -0,0 +1,17 @@
+package algo;
+
+public class TitleCase {
+
+    public String titleCase(String text){
+        String [] words = text.split("[\\s]+");
+        StringBuilder builder;
+
+        for(int index = 0; index < words.length; index++){
+            builder = new StringBuilder(words[index].substring(0, 1).toUpperCase());
+            builder.append(words[index].substring(1).toLowerCase());
+            words[index] = builder.toString();
+        }
+
+        return String.join(" ", words);
+    }
+}
