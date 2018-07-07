22f6b7d9a6d82e535ba062bcb543d810ad79997b
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 19:19:26 +0200

    Write algoritm to count largest possible product of three numbers from given array using brutal force technique

--- src/main/java/algo/HighestIntProduct.java
+++ src/main/java/algo/HighestIntProduct.java
@@ -87,4 +87,32 @@ public class HighestIntProduct {
         return maxPositiveProduct > maxNegativeProduct ? maxPositiveProduct : maxNegativeProduct;
     }
 
+    public long getMaxProductofThreeWithNestedLoop(int [] numbers){
+        Integer maxProduct = null;
+
+        for(int index1 = 0; index1 < numbers.length; index1++){
+            for(int index2 = 0; index2 < numbers.length; index2++){
+                if(index1 == index2){
+                    continue;
+                }
+
+                for(int index3 = 0; index3 < numbers.length; index3++){
+                    if((index2 == index3) || (index1 == index3)){
+                        continue;
+                    }
+
+                    int result = numbers[index1] * numbers[index2] * numbers[index3];
+                    if( maxProduct == null){
+                        maxProduct = result;
+                    }else if(result > maxProduct) {
+                        maxProduct = result;
+                    }
+
+                }
+            }
+        }
+        return maxProduct;
+    }
+
+
 }
