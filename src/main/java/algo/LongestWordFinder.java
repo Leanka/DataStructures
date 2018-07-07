0820a48177d38325cf83a962c71c21dd15e7e3c8
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 18:30:29 +0200

    Add validation of text length, throw exception if text is empty

--- src/main/java/algo/LongestWordFinder.java
+++ src/main/java/algo/LongestWordFinder.java
@@ -5,6 +5,10 @@ import algo.helper.Tuple;
 public class LongestWordFinder {
 
     public Tuple findLongestWord(String text){
+        if(text.length() == 0){
+            throw new IllegalArgumentException("Text cannot be empty");
+        }
+
         String [] words = text.split("[\\s]+");
         String longestWord = "";
         for(String word:words){
