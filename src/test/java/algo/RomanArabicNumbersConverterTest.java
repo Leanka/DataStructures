a1986389cc47214a4f6f5a51fa1a1a1f3fceebcb
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 16:50:28 +0200

    Test converting roman numbers in ascending, descending and random order to arabic ones

---
+++ src/test/java/algo/RomanArabicNumbersConverterTest.java
@@ -0,0 +1,41 @@
+package algo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RomanArabicNumbersConverterTest {
+    private RomanArabicNumbersConverter converter;
+
+    @Before
+    public void setUp(){
+        converter = new RomanArabicNumbersConverter();
+    }
+
+    @Test
+    public void testConvertingRomanNumbersToArabic(){
+        String romanNumber = "MCDX";
+        int arabicNumber = 1410;
+
+        assertEquals(arabicNumber, converter.romanToArabic(romanNumber));
+    }
+
+    @Test
+    public void testConvertingRomanAscendingNumbersToArabic(){
+        String romanNumber = "MDCCLXXXIX";
+        int arabicNumber = 1789;
+
+        assertEquals(arabicNumber, converter.romanToArabic(romanNumber));
+    }
+
+    @Test
+    public void testConvertingRomanDescendingNumbersToArabic(){
+        String romanNumber = "MCMLXXXVII";
+        int arabicNumber = 1987;
+
+        assertEquals(arabicNumber, converter.romanToArabic(romanNumber));
+    }
+    
+
+}
