c61a9358669b53154ef187529c06182d461eb139
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 22:43:32 +0200

    Test sorting randomly ordered numbers with duplicates

--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -22,4 +22,13 @@ public abstract class SortTest {
         assertArrayEquals(sortedNumbers, numbers);
     }
 
+    @Test
+    public void testSortingRandomElementsWithRepetition(){
+        int [] numbers = {2, 3, 6, 7, 10, 1, 9, 3, 7, 10, 5};
+        int [] sortedNumbers = {1,2,3,3,5,6,7,7,9,10, 10};
+
+        numbers = sortIntegers.intSort(numbers);
+
+        assertArrayEquals(sortedNumbers, numbers);
+    }
 }
