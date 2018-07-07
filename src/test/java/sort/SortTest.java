6e8443d785f3e773d37a0951a519cf48d2109ed2
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 22:42:51 +0200

    Test sorting randomly ordered numbers

--- src/test/java/sort/SortTest.java
+++ src/test/java/sort/SortTest.java
@@ -11,5 +11,15 @@ public abstract class SortTest {
 
         this.sortIntegers = sortIntegers;
     }
-    
+
+    @Test
+    public void testSortingRandomElements(){
+        int [] numbers = {2, 4, 6, 8, 10, 1, 9, 3, 7, 5};
+        int [] sortedNumbers = {1,2,3,4,5,6,7,8,9,10};
+
+        numbers = sortIntegers.intSort(numbers);
+
+        assertArrayEquals(sortedNumbers, numbers);
+    }
+
 }
