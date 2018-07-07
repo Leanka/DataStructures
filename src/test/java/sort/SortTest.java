c287afe21ec10879351a8db0461b42a408122af5
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 22:41:41 +0200

    Create abstract class for testing sorts

---
+++ src/test/java/sort/SortTest.java
@@ -0,0 +1,15 @@
+package sort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public abstract class SortTest {
+    private Sort sortIntegers;
+
+    protected void setSortIntegers(Sort sortIntegers) {
+
+        this.sortIntegers = sortIntegers;
+    }
+    
+}
