040420cb69122a56f3b5f1195f94fa3023073a39
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 22:53:54 +0200

    Test helper insertValueBackwords() on regular and corner case insertion

--- src/test/java/sort/InsertionSortTest.java
+++ src/test/java/sort/InsertionSortTest.java
@@ -12,6 +12,26 @@ public class InsertionSortTest extends SortTest{
         setSortIntegers(new InsertionSort());
     }
 
+    @Test
+    public void testInsertingValue(){
+        InsertionSort insertionSort = new InsertionSort();
 
+        int [] numbers = {5, 6, 7, 8, 9, 10, 11, 12, 13};
+        int [] numbersAfterInsert = {5, 12, 6, 7, 8, 9, 10, 11, 13};
+
+        insertionSort.insertValueBackwords(numbers, 1, 7);
+        assertArrayEquals(numbersAfterInsert, numbers);
+    }
+
+    @Test
+    public void testInsertingFromLastToFirstPosition(){
+        InsertionSort insertionSort = new InsertionSort();
+
+        int [] numbers = {5, 6, 7, 8, 9, 10};
+        int [] numbersAfterInsert =  {10, 5, 6, 7, 8, 9};
+
+        insertionSort.insertValueBackwords(numbers, 0, 5);
+        assertArrayEquals(numbersAfterInsert, numbers);
+    }
 
 }
