75d145b48b51d5a40dfe978298de3d883174776e
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 17:09:07 +0200

    Implement Calculating given fibonacci number using single loop iteration

--- src/main/java/algo/Fibonacci.java
+++ src/main/java/algo/Fibonacci.java
@@ -32,5 +32,24 @@ public class Fibonacci {
         }
     }
 
+    public long calculateWithLoop(int number){
+        if(number < 0){
+            throw new NumberFormatException("Negative value");
+        }
+        if((number == 1) | (number == 0)){
+            return number;
+        }
+        int first = 0;
+        int next = 1;
+        int current = 0;
+        for(int counter = 1; counter < number; counter++){ //starts at 3th number, as 1st and 2nd are bound to vars above
+            current = first + next;
+            first = next;
+            next = current;
+        }
+
+        return current;
+    }
+
 
 }
