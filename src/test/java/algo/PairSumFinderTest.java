191462db6fd759ac99a1f6a93d9b40dea2ac6341
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 16:58:36 +0200

    Test finding a pair with given sum including positive and negative values

---
+++ src/test/java/algo/PairSumFinderTest.java
@@ -0,0 +1,40 @@
+package algo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PairSumFinderTest {
+    private PairSumFinder checker;
+
+    @Before
+    public void setUp(){
+        checker = new PairSumFinder();
+    }
+
+    @Test
+    public void testSumFinding(){
+        int [] numbers = {8, 2, 15, 1};
+        int sum = 17;
+
+        assertTrue(checker.findPairWithGivenSum(numbers, sum));
+    }
+
+    @Test
+    public void testSumFindingWithNegatives(){
+        int [] numbers = {8, 2, 15, 1, -4, 6, -21, 0, 7};
+        int sum = -20;
+
+        assertTrue(checker.findPairWithGivenSum(numbers, sum));
+    }
+
+    @Test
+    public void testSumFindingOnlyPositives(){
+        int [] numbers = {8, 2, 15, 1, 4, 6, 21, 0, 7};
+        int sum = 8;
+
+        assertTrue(checker.findPairWithGivenSum(numbers, sum));
+    }
+
+}
