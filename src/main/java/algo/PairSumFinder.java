fcf9259708457b2d6bae8af941d972a59bfc50ce
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 17:00:51 +0200

    Implement algoritm to find Pair of numbers with given sum

---
+++ src/main/java/algo/PairSumFinder.java
@@ -0,0 +1,31 @@
+package algo;
+
+import java.util.Arrays;
+
+public class PairSumFinder {
+
+    public boolean findPairWithGivenSum(int [] numbers, int sum){
+        if(numbers.length < 2){
+            throw new IllegalArgumentException("Array shorter than 2 are not allowed");
+        }
+        Arrays.sort(numbers);
+
+        int firstIndex = 0;
+        int lastIndex = numbers.length - 1;
+        boolean isSumPresent = false;
+
+        do{
+            int currentSum = numbers[firstIndex] + numbers[lastIndex];
+            if(currentSum == sum){
+                isSumPresent = true;
+                break;
+            }else if(currentSum > sum){
+                lastIndex--;
+            }else if(currentSum < sum){
+                firstIndex++;
+            }
+
+        }while (firstIndex < lastIndex);
+        return isSumPresent;
+    }
+}
