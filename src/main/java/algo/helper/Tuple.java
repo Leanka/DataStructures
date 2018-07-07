50a8cefe7e4b8dd787d3cebc11d6691f6186dda2
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 18:12:51 +0200

    Implement Tuple wrapper class for LongestWordFinder product

---
+++ src/main/java/algo/helper/Tuple.java
@@ -0,0 +1,33 @@
+package algo.helper;
+
+import java.util.Objects;
+
+public class Tuple {
+    private String word;
+
+    public Tuple(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getWordLength(){
+        return word.length();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tuple)) return false;
+        Tuple tuple = (Tuple) o;
+        return Objects.equals(getWord(), tuple.getWord());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getWord());
+    }
+}
