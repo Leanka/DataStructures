1cbf2ef27214e24291d664a3195c46d02236edaf
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 17:03:36 +0200

    Test calculationg fibonacci number using algorytm based on recursion

---
+++ src/test/java/algo/FibonacciTest.java
@@ -0,0 +1,29 @@
+package algo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @Before
+    public void setUp(){
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    public void testFibonnaciFor8(){
+        int number = 8;
+        long fibonacciNumber = 21;
+
+        assertEquals(fibonacciNumber, fibonacci.calculateRecursiveSequence(number));
+    }
+
+
+
+}
