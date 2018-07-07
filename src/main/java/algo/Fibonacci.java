a23788718baef1e74a6548109eb7695b49e4f9a8
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-07 17:07:36 +0200

    Implement Calculating given fibonacci number using memoization

--- src/main/java/algo/Fibonacci.java
+++ src/main/java/algo/Fibonacci.java
@@ -14,7 +14,23 @@ public class Fibonacci {
         return calculateRecursiveSequence(number-1) + calculateRecursiveSequence(number-2);
     }
 
+    public long calculateWithMap(int number, Map<Integer, Long> fiboPairs){
+        if(number < 0){
+            throw new NumberFormatException("Negative value");
+        }
 
+        if((number == 1) | (number == 0)){
+            return number;
+        }
+
+        if(fiboPairs.containsKey(number)){
+            return fiboPairs.get(number);
+        }else {
+            long result = calculateWithMap(number-2, fiboPairs) + calculateWithMap(number-1, fiboPairs);
+            fiboPairs.put(number, result);
+            return result;
+        }
+    }
 
 
 }
