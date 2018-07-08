a13b65cb85aa42826856d10426faa6a5f4803d5e
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-08 11:40:32 +0200

    Test prepending in linked list

--- src/test/java/dataStructures/linkedList/LinkedListTest.java
+++ src/test/java/dataStructures/linkedList/LinkedListTest.java
@@ -17,7 +17,26 @@ public class LinkedListTest {
         assertEquals(expectedListContent, customLinkedList.toString());
     }
 
+    @Test
+    void prependTest() {
+        LinkedList<Integer> customLinkedList = createLinkedList(10);
+        customLinkedList.prepend(33);
+        customLinkedList.prepend(114);
+        String expectedListContent = " 114 33 0 1 2 3 4 5 6 7 8 9";
+        assertEquals(expectedListContent, customLinkedList.toString());
+    }
+
+    @Test
+    void prependOnEmptyLinkedListTest() {
+        LinkedList<Integer> customLinkedList = new CustomLinkedList<>();
+        customLinkedList.prepend(33);
+        customLinkedList.prepend(114);
+        String expectedListContent = " 114 33";
+        assertEquals(expectedListContent, customLinkedList.toString());
+    }
+
     
+
     private LinkedList<Integer> createLinkedList(int numOfElements) {
         LinkedList<Integer> customLinkedList = new CustomLinkedList<>();
         for (int i = 0; i < numOfElements; ++i)
