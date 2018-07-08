9b250f86023aef62fe29a27bb3716e1fcfc3afc2
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-08 11:33:37 +0200

    Implement dynamic array interface for raw types

---
+++ src/main/java/dataStructures/dynamicArray/raw/DynamicArray.java
@@ -0,0 +1,9 @@
+package dataStructures.dynamicArray.raw;
+
+public interface DynamicArray {
+    void add(int value);
+    void remove(int value);
+    void insert(int index, int value);
+    int length();
+    String toString();
+}
