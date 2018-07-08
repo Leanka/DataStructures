f9b0a8f27c831a0a52c0f81cd873ad3bb22cac18
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-08 12:03:43 +0200

    Test adding element to queue; Test extending queue capacity feature

---
+++ src/test/java/dataStructures/queue/CustomQueueTest.java
@@ -0,0 +1,66 @@
+package dataStructures.queue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomQueueTest {
+    private Queue <Integer> queue;
+    private  int initialCapacity = 2;
+
+    @Before
+    public void setUpDataBeforeTest(){
+        queue = new CustomQueue <> ();
+    }
+
+    @Test
+    public void testIsNewQueueEmpty(){
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testAddingOneElementToQueue(){
+        queue.enqueue(5);
+        assertEquals(1, queue.getTail());
+    }
+
+
+
+    private void addElementsToQueue(int times){
+        while (times-- > 0){
+            queue.enqueue(times);
+        }
+    }
+
+
+    @Test
+    public void testCapacityDoubling(){
+        int expectedFinalCapacity = initialCapacity * 2;
+        addElementsToQueue(initialCapacity + 1);
+        assertEquals(expectedFinalCapacity, queue.getCapacity());
+    }
+
+    @Test
+    public void testCapacityMultiplying(){
+        int countOfCapacityExtending = 5;
+        int expectedFinalCapacity = getMultiplyedCapacity(countOfCapacityExtending);
+
+        int numberOfElementsToForceCapacityExtension = getMultiplyedCapacity(countOfCapacityExtending-1) + 1;
+        addElementsToQueue(numberOfElementsToForceCapacityExtension);
+
+        assertEquals(expectedFinalCapacity, queue.getCapacity());
+    }
+
+    private int getMultiplyedCapacity(int times){
+        int capacityMultiplication = 2;
+        int newCapacity = initialCapacity;
+
+        while (times-- > 0){
+            newCapacity *= capacityMultiplication;
+        }
+        return newCapacity;
+    }
+
+
+}
