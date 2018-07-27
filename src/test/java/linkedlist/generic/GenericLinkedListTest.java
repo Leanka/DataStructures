7bfa5e12d5620abdfffbe45566f6a6d617275ff7
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-27 12:58:52 +0200

    Implement set of tests for generic version of linked list implementation

---
+++ src/test/java/linkedlist/generic/GenericLinkedListTest.java
@@ -0,0 +1,88 @@
+package linkedlist.generic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GenericLinkedListTest {
+
+    @Test
+    void classExistanceTest(){
+        new GenericLinkedList<String>();
+    }
+
+    @Test
+    void appendTest() {
+        LinkedList <Integer> linkedList = createLinkedList(10);
+        String expectedListContent = " 0 1 2 3 4 5 6 7 8 9";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void prependTest() {
+        LinkedList <Integer> linkedList = createLinkedList(10);
+        linkedList.prepend(33);
+        linkedList.prepend(114);
+        String expectedListContent = " 114 33 0 1 2 3 4 5 6 7 8 9";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void prependOnEmptyLinkedListTest() {
+        LinkedList <Integer> linkedList = new GenericLinkedList();
+        linkedList.prepend(33);
+        linkedList.prepend(114);
+        String expectedListContent = " 114 33";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void removeElementTest() {
+        LinkedList <Integer> linkedList = createLinkedList(10);
+        linkedList.delete(5);
+        linkedList.delete(2);
+        String expectedListContent = " 0 1 3 4 6 7 8 9";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void removeElementOnEmptyLinkedListTest() {
+        LinkedList <Integer> linkedList = new GenericLinkedList();
+        linkedList.delete(5);
+        linkedList.delete(2);
+        String expectedListContent = "";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void removeFirstElementTest() {
+        LinkedList <Integer> linkedList = createLinkedList(10);
+        linkedList.delete(0);
+        String expectedListContent = " 1 2 3 4 5 6 7 8 9";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void removeLastElementTest() {
+        LinkedList <Integer> linkedList = createLinkedList(10);
+        linkedList.delete(9);
+        String expectedListContent = " 0 1 2 3 4 5 6 7 8";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    @Test
+    void removeElementThatNotExistsTest() {
+        LinkedList <Integer> linkedList = createLinkedList(10);
+        linkedList.delete(15);
+        String expectedListContent = " 0 1 2 3 4 5 6 7 8 9";
+        assertEquals(expectedListContent, linkedList.toString());
+    }
+
+    private LinkedList <Integer> createLinkedList(int numOfElements) {
+        LinkedList <Integer> array = new GenericLinkedList<>();
+        for (int i = 0; i < numOfElements; ++i)
+        {
+            array.append(i);
+        }
+        return array;
+    }
+}
