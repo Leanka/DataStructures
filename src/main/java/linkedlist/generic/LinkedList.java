2ada649417cd868b7eb8a56b7ef4da3c56f0a416
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-27 12:57:34 +0200

    Implement linked list interface

---
+++ src/main/java/linkedlist/generic/LinkedList.java
@@ -0,0 +1,9 @@
+package linkedlist.generic;
+
+public interface LinkedList <T>{
+        void append(T data);
+        void prepend(T data);
+        void delete(T data);
+        String toString();
+
+    }
