59b12cd94960537f3394c8aea93f42ae8ec08a56
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-07-27 12:12:14 +0200

    Implement generic dynamic arrays interfacte

---
+++ src/main/java/dynamicArray/DynamicArray.java
@@ -0,0 +1,9 @@
+package dynamicArray;
+
+public interface DynamicArray <T> {
+    void add(T value);
+    void remove(T value);
+    void insert(int index, T value);
+    int length();
+    String toString();
+}
